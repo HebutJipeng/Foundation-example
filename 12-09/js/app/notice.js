e6e8651ad6bd112ce3d9d4fff42bbaa17648da98
HebutJipeng <282602893@qq.com>
2016-12-27 20:08:16 +0800

    27

--- 12-09/js/app/notice.js
+++ 12-09/js/app/notice.js
@@ -1,8 +1,15 @@
-function alertMsg(msg) {
+function alertMsg(msg, callback) {
 	var _msg = "<div class='alertMsg'>" + msg + "</div>";
 	$("body").append(_msg);
 	$(".alertMsg").show();
 	setTimeout(function() {
-		$(".alertMsg").fadeOut();
+		$(".alertMsg").fadeOut('normal', function() {
+			$(this).remove();
+			if (callback) {
+				if (typeof callback == 'function') {
+					callback()
+				}
+			}
+		});
 	}, 2000)
 }
